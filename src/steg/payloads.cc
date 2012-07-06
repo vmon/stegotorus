e3de49b3226cc4b092a9776e80f707f6b47b5400
vmon <vmonmoonshine@gmail.com>
2012-07-06 09:10:04 -0600

    - Resolving embarrassing compiling error which made Zack uncomfortable. - gitignore to ignore the auto-generated source files which are cc files now

--- src/steg/payloads.cc
+++ src/steg/payloads.cc
@@ -1293,7 +1293,7 @@ init_PDF_payload_pool(payloads& pl, int len, int type, int minCapacity)
       log_debug("got pdf (index %d) with capacity %d", r, cap);
       if (cap > minCapacity) {
 	log_debug("pdf (index %d) greater than mincapacity %d", cnt, minCapacity);
-	pl.ypePayloadCap[contentType][cnt] = (cap-PDF_DELIMITER_SIZE)/2;
+	pl.typePayloadCap[contentType][cnt] = (cap-PDF_DELIMITER_SIZE)/2;
 	pl.typePayload[contentType][cnt] = r;
 	cnt++;
 	
